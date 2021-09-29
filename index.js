1ca768f1e13c627c2eb454bf867e6ea38ce2ce73
AmitFikler <amitfikler@gmail.com>
2021-09-29 20:18:59 +0300

    💾Continue saving information from the local storage user

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ addForm.style.display = "none"
 const addProdact = document.getElementById("add-prodact")
 const table = document.getElementsByTagName("table")[0]
 const addRoommate = document.getElementById("add-roommate");
+const radioDiv = document.getElementById("radio-inputs")
+
 
 
 
@@ -12,18 +14,27 @@ const addRoommate = document.getElementById("add-roommate");
 if (JSON.parse(localStorage.getItem("roommates")) === null) {
     localStorage.setItem("roommates","[]")
 }
-localStorageRoommates = JSON.parse(localStorage.getItem("roommates"))
+let localStorageRoommates = JSON.parse(localStorage.getItem("roommates"))
+
+
+if (JSON.parse(localStorage.getItem("tableRows")) === null) {
+    localStorage.setItem("tableRows","[]")
+}
+let localStorageTablerows = JSON.parse(localStorage.getItem("tableRows"))
+
 retriveRoommates()
 
 
 plusBtn.addEventListener("click",()=> {
     if (addForm.style.display === "none") {
         plusBtn.textContent = "x"
+        plusBtn.style.backgroundColor = "#ff6161"
         addForm.style.display = "block"
     }
     else {
         plusBtn.textContent = "+"
         addForm.style.display = "none"
+        plusBtn.style.backgroundColor = "rgb(138, 224, 138)"
     }
 
 })
@@ -58,6 +69,7 @@ function addNewRoommate() {
         else{
             inputName.textContent = document.getElementById("new-roommate").value
             localStorageRoommates.push(inputName.textContent)
+            addRadio(inputName.textContent)
             localStorage.setItem("roommates", JSON.stringify(localStorageRoommates))
         }
     }
@@ -66,7 +78,7 @@ function addNewRoommate() {
 
 function appendTr() {
     const what = document.getElementById("what").value
-    const who = document.getElementById("who").value
+    const who = document.querySelector('input[name="roommate"]:checked').value;
     const how = document.getElementById("how").value
 
 
@@ -80,17 +92,37 @@ function appendTr() {
     cel1.innerHTML = what;
     cel2.innerHTML = `${how}$`;
     cel3.innerHTML = who;
+
+    localStorageTablerows.push({"what":what,"how":how,"who":who})
+    localStorage.setItem("tableRows", JSON.stringify(localStorageTablerows))
 }
 
 
 
 function retriveRoommates() {
     for(let roommate of localStorageRoommates) {
+        addRadio(roommate)
         const newLi = document.createElement("li")
         newLi.textContent = roommate
         leftNavbar.insertBefore(newLi,leftNavbar.firstChild)     
     }
 }
+
+function addRadio(str) {
+    const radioInput = document.createElement("input")
+    radioInput.type = "radio"
+    radioInput.id = `radio-${str}`
+    radioInput.name = `roommate`
+    radioInput.value = str
+    const label  = document.createElement("label")
+    label.for = `radio-${str}`
+    label.textContent = str
+    radioDiv.appendChild(radioInput)
+    radioDiv.appendChild(label)
+}
+
+
+
 
 
 
