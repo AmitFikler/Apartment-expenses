82feb3226490df043945eeb5f78de8f33446995b
AmitFikler <amitfikler@gmail.com>
2021-09-30 15:11:36 +0300

    Finished project

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Variables I use
+
 const plusBtn = document.getElementById("plus")
 const addForm = document.getElementById("form")
 const leftNavbar = document.getElementById("left-navbar")
@@ -10,6 +12,7 @@ const radioDiv = document.getElementById("radio-inputs")
 
 
 
+// set and get from local storage
 
 if (JSON.parse(localStorage.getItem("roommates")) === null) {
     localStorage.setItem("roommates","[]")
@@ -22,13 +25,17 @@ if (JSON.parse(localStorage.getItem("tableRows")) === null) {
 }
 let localStorageTablerows = JSON.parse(localStorage.getItem("tableRows"))
 
+
+// Build the Dom
 for(let shop of localStorageTablerows) {
     retriveShoppingList(shop)
 }
 retriveRoommates()
+totalExpenses()
 
 
 
+// eventlisteners!! 
 
 plusBtn.addEventListener("click",()=> {
     if (addForm.style.display === "none") {
@@ -44,22 +51,30 @@ plusBtn.addEventListener("click",()=> {
 
 })
 
-
 addProdact.addEventListener("click",appendTr)
-
 
 addRoommate.addEventListener("click", addNewRoommate)
 
 document.body.addEventListener("contextmenu", (e) => {
-    if(e.target.tagName === "LI") {
+    if(e.target.tagName === "LI" && e.target["id"] !== "add-roommate") {
         e.preventDefault();
         localStorageRoommates.splice(localStorageRoommates.indexOf(e.target.textContent), 1)
+        removeRadio(e.target.textContent)
+        removeTableRowByName(e.target.textContent)
         localStorage.setItem("roommates", JSON.stringify(localStorageRoommates))
         e.target.remove()
     }
 })
 
+document.body.addEventListener("mousedown", mousedown)
 
+document.body.addEventListener("mouseup", MouseUp)
+
+document.body.addEventListener("dblclick", removeTr)
+
+
+
+// functions
 function addNewRoommate() {
     inputName = document.createElement("li")
     inputName.innerHTML = "<input type='text' id='new-roommate'>"
@@ -91,8 +106,6 @@ function appendTr() {
     localStorage.setItem("tableRows", JSON.stringify(localStorageTablerows))
 }
 
-
-
 function retriveRoommates() {
     for(let roommate of localStorageRoommates) {
         addRadio(roommate)
@@ -115,9 +128,11 @@ function retriveShoppingList({what,how,who}) {
     cel3.innerHTML = who;
 
     totalExpenses()
+    colorEven()
 }
 
 function addRadio(str) {
+    document.getElementById("remove-me").style.display = "none"
     const radioInput = document.createElement("input")
     radioInput.type = "radio"
     radioInput.id = `radio-${str}`
@@ -130,20 +145,102 @@ function addRadio(str) {
     radioDiv.appendChild(label)
 }
 
+function removeRadio(str) {
+    for (let i of radioDiv.children) {
+        if(i.value === str) {
+            i.remove()
+        }
+    }
+    for(let i of radioDiv.children) {
+        if(i.textContent === str) {
+            i.remove()
+        }
+    }
+    if(radioDiv.children.length === 1){
+        document.getElementById("remove-me").style.display = "block"
+    }
+}
 
 function calculation() {
     const prices = []
     for (let i = 1; i<document.querySelectorAll("tr").length; i++) {
-        const price = document.querySelectorAll("tr")[i].children[1].textContent.slice(0,-1)
-        prices.push(Number(price))
+        if (document.querySelectorAll("tr")[i].style.display !== "none") {
+            const price = document.querySelectorAll("tr")[i].children[1].textContent.slice(0,-1)
+            prices.push(Number(price))
+        }
     }
     return prices.reduce((a,b) => a + b, 0)  
 }
 
-
 function totalExpenses() {
     document.getElementById("total").textContent = `${calculation()}$`
 }
+
+function colorEven() {
+    let counter = 1
+    for(let tr of document.querySelectorAll("tr")) {
+        if (counter % 2  === 0){
+            tr.style.backgroundColor = "rgb(211, 211, 211)"
+        }
+        else {
+            tr.style.backgroundColor = "rgb(235, 235, 235)"
+        }
+        counter++
+    }
+}
+
+function mousedown(e) {
+    if(e.target.tagName === "LI" && e.target["id"] !== "add-roommate"){
+        for(let tr of document.querySelectorAll("tr")){
+            if(tr.children[0].textContent !== "What did you buy?"){
+                if (tr.children[2].textContent !== e.target.textContent) {
+                    tr.style.display = "none"
+                }
+            }
+        }
+        totalExpenses()
+    }
+}
+
+function MouseUp(e) {
+    if(e.target.tagName === "LI" && e.target["id"] !== "add-roommate"){
+        for(let tr of document.querySelectorAll("tr")) {
+            tr.style.display = ""
+        }
+    }
+    totalExpenses()
+}
+
+function removeTr(e) {
+    if (e.target.tagName === "TD") {
+        e.target.parentElement.remove()
+    }
+    totalExpenses()
+    watchTable()
+}
+
+function watchTable() {
+    let tableRow  = []
+    for (let i = 1; i<document.querySelectorAll("tr").length; i++) {
+        if (document.querySelectorAll("tr")[i].style.display !== "none") {
+            tableRow.push({"what":document.querySelectorAll("tr")[i].children[0].textContent, "how":document.querySelectorAll("tr")[i].children[1].textContent.slice(0,-1),"who":document.querySelectorAll("tr")[i].children[2].textContent})
+        }
+    }
+    localStorageTablerows = tableRow
+    localStorage.setItem("tableRows", JSON.stringify(localStorageTablerows))
+}
+
+function removeTableRowByName(str) {
+    for(let tr of document.querySelectorAll("tr")){
+        if (tr.children[2].textContent === str) {
+            tr.remove()
+        }
+    }
+    totalExpenses()
+    watchTable()
+}
+
+
 
 
 
